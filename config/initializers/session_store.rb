bbe3df8b0b0ce392b66f5f282e27caf155cf49a1
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-03 12:53:41 -0500

    Updating with rake rails:ups ate

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-NolaOnRails::Application.config.session_store :cookie_store, key: '_nolaonrails_session'
+NolaOnRails::Application.config.session_store :cookie_store, key: '_nola_on_rails_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
