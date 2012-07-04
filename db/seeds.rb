f8067390610e72af5a6763fa6996d6fba9a2f7a0
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-04 18:27:54 -0500

    Setting up a preliminary seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Account.delete_all
+Course.delete_all
+Session.delete_all
+
+teachers = (1..rand(10..100)).map do
+  teacher = FactoryGirl.create :teacher
+  courses = (1..rand(1..5)).map do
+    course = FactoryGirl.create :course
+    students = (0..rand(0..10)).map do
+      FactoryGirl.create :student
+    end
+    course.enrollments << students
+    course
+  end
+  teacher.courses << courses
+end
