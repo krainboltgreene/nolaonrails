9585d72aee0d4d83694057e25942e9428d5ecfa0
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-04 18:21:19 -0500

    Showing the start and end dates for the course

--- app/decorators/course_decorator.rb
+++ app/decorators/course_decorator.rb
@@ -1,4 +1,21 @@
 class CourseDecorator < Draper::Base
   decorates :course
   decorates_association :accounts
+
+  def start_at_date
+    if sessions.any?
+
+    else
+      "---"
+    end
+  end
+
+  def end_at_date
+    if sessions.any?
+
+    else
+      "---"
+    end
+  end
+
 end
