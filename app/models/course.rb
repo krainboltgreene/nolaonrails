4388189973866037be8669df5fa71a8f963939d9
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-04 19:06:47 -0500

    Adding the belongs to for teacher to account

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ActiveRecord::Base
   has_and_belongs_to_many :enrollments, join_table: "enrollments", class_name: "Account"
   has_many :sessions
+  belongs_to :teacher, class_name: "Account"
 
   mount_uploader :image, ImageUploader
 
