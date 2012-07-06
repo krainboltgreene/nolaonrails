ce487c33718872c13acdc54f044e69934581f5b3
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-06 17:37:18 -0500

    Adding some help text for the credit card form.

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -17,4 +17,13 @@ module AccountsHelper
       input_html: { class: "span1" }
     }
   end
+
+  def card_form_text
+    """
+    We'll use your card only when you enrole in a course.
+    We don't store your card information on our servers.
+    Your card information is sent securely to a payment processor.
+    #{link_to "Read more details here.", controller: :pages, action: :policies}
+    """
+  end
 end
