ca06825166a03887cb626e822bc1ce2482aa24f1
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-11 18:19:49 -0500

    Fixing the missing account for the activation mailer

--- app/mailers/activation_mailer.rb
+++ app/mailers/activation_mailer.rb
@@ -2,6 +2,7 @@ class ActivationMailer < ActionMailer::Base
   default from: "noreply@nolaonrails.com"
 
   def activation_needed_email(account)
+    @account = account
     mail to: account.email
   end
 end
