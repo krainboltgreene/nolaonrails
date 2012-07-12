68ed2a94c72c1dda90b9b643ede5b50e6dfad241
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-11 21:46:22 -0500

    Requring card for all controllers, creating require footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :require_header
-  before_filter -> { @footer = true }
   before_filter :require_login
-
+  before_filter :require_card
+  before_filter :require_header
+  before_filter :require_footer
 
   private
 
@@ -20,5 +20,9 @@ class ApplicationController < ActionController::Base
 
   def require_header
     @header = true
+  end
+
+  def require_footer
+    @footer = true
   end
 end
