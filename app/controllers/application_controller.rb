c8604c33e0ace184545894ff2b8077dfe12bfd9e
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-11 21:45:08 -0500

    creating a require credit card filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,13 @@ class ApplicationController < ActionController::Base
     AccountDecorator.new current_user
   end
 
+  def require_card
+    unless current_account.stripe_customer_token.present?
+      flash[:error] = "You need to enter your card details."
+      redirect_to card_account_path current_account
+    end
+  end
+
   def require_header
     @header = true
   end
