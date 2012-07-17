3808d8714922eb428fb6b9af06f09242f2b90faf
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-07-17 15:47:43 -0500

    Skipping login for index and show

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,6 @@
 class CoursesController < ApplicationController
+  skip_before_filter :require_login, only: [:index, :show]
+
   def index
     @courses = if params[:account_id]
       current_account.courses
